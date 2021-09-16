02711e6b4b1f5cabb9fc76fc18f68a53d2cfd0d6
Nawzneen <86608888+Nawzneen@users.noreply.github.com>
2021-09-16 15:40:27 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date(2021, 10, 26, 11, 43).getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("text-message").innerHTML = "EXPIRED";
+  }
+}, 1000);
